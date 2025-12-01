9651c7367789b04d4775b3346de3f1378c168f5c
maozo345 <maozolamy@gmail.com>
2025-12-01 20:41:33 +0200

    feat: Add animations and update styles
    Introduces CSS animations for a more dynamic user experience and updates the background color to a darker shade for improved visual contrast. Also refactors React root rendering for cleaner setup.

--- index.tsx
+++ index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -7,9 +6,4 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(rootElement).render(<App />);
